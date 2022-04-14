091a4c1a02c4ae042582153ae16cafb31965c49e
SMorrill21 <sethmorrill2010@gmail.com>
2022-04-14 11:42:11 -0400

    Upload New Changes

---
+++ Test Hive (Oculus 2 Testing)/Assets/HandPressence.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPressence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            //GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            //if (prefab)
+            //{
+                //spawnedController = Instantiate(prefab, transform);
+            //}
+            //else
+            //{
+                Debug.Log("no");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            //}
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+
+        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2dAxisValue);
+    }
+}
